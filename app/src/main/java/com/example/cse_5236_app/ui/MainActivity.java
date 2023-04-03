3701b1d2fa172b9df8e6e060cad773fab084abd2
JJKong20 <20kongjj@gmail.com>
2023-04-03 03:09:53 -0400

    comment cleanup

--- app/src/main/java/com/example/cse_5236_app/ui/MainActivity.java
+++ app/src/main/java/com/example/cse_5236_app/ui/MainActivity.java
@@ -71,19 +71,7 @@ public class MainActivity extends AppCompatActivity {
                 return false;
             }
         });
-        /*
-        Bundle bundle = new Bundle();
-        bundle.putString("userid", userid);
-        View view = findViewById(R.id.nav_host_fragment_activity_main);
-        BottomNavigationView navView = findViewById(R.id.nav_view);
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
-        AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications)
-                .build();
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_main);
-        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
-        NavigationUI.setupWithNavController(binding.navView, navController);*/
+
 
     }
 
